f50a5aefcfc42fe0b0836df8958891a2975b549c
aarricgra <aarricgra@alu.edu.gva.es>
2023-03-20 08:22:09 +0100

    a

---
+++ TextBox/src/TextBox.java
@@ -0,0 +1,14 @@
+class TextBox {
+    String texto;
+    int ancho, alto;
+
+    @Override
+    public String toString() {
+        return ancho + "x" + alto + "\n" +
+                "┏" + "━".repeat(ancho) + "┓\n" +
+                ("┃" + " ".repeat(ancho) + "┃\n").repeat((alto - 1) / 2) +
+                (alto > 0 ? "┃" + " ".repeat((ancho - texto.length() + 1) / 2) + texto + " ".repeat((ancho - texto.length()) / 2) + "┃\n" : "") +
+                ("┃" + " ".repeat(ancho) + "┃\n").repeat(alto / 2) +
+                "┗" + "━".repeat(ancho) + "┛\n";
+    }
+}
