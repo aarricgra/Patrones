13613e96a47939e6809b549af9fa3015833d72ac
aarricgra <aarricgra@alu.edu.gva.es>
2023-04-21 09:53:50 +0200

    a

--- SendNotification/src/Main.java
+++ SendNotification/src/Main.java
@@ -7,28 +7,44 @@ abstract class Notification{
     abstract void sendNotification();
     static Notification factory(String destination){
         if(destination.contains(" ")){
-            return new SMSNotification();
+            return new SMSNotification(destination);
         } else if (destination.substring(0,4).equals("http")) {
-            return new PushNotification();
+            return new PushNotification(destination);
         }else{
-            return new EmailNotification();
+            return new EmailNotification(destination);
         }
     }
 }
 
 class SMSNotification extends Notification{
+    String destination;
+
+    public SMSNotification(String destination) {
+        this.destination = destination;
+    }
+
     @Override
     void sendNotification() {
         System.out.println("Sending sms to: "+destination);
     }
 }
 class EmailNotification extends Notification{
+    String destination;
+
+    public EmailNotification(String destination) {
+        this.destination = destination;
+    }
     @Override
     void sendNotification() {
         System.out.println("Sending email to: "+destination);
     }
 }
 class PushNotification extends Notification{
+    String destination;
+
+    public PushNotification(String destination) {
+        this.destination = destination;
+    }
     @Override
     void sendNotification() {
         System.out.println("Sending push to: "+destination);
